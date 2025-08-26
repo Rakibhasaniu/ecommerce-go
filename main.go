c469fb9ec5d06928775cc770fa7ba7f1a435ce22
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2025-08-27 00:04:23 +0600

    route added

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 import "net/http"
+import "encoding/json"
 
 
 func helloHandler(w http.ResponseWriter, r *http.Request){
@@ -10,6 +11,30 @@ func helloHandler(w http.ResponseWriter, r *http.Request){
 }
 func aboutHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintln(w, "About page")
+}
+
+type Product struct {
+	ID int `json:"rakib_id"`
+	Title string
+	Price float64
+	Description string
+	ImgUrl string
+}
+
+var productList  []Product
+
+func getProductsHandler( w http.ResponseWriter, r *http.Request){
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	encoder :=json.NewEncoder(w)
+	err := encoder.Encode(productList)
+	if err != nil {
+		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main(){
@@ -20,6 +45,7 @@ func main(){
 
 	mux.HandleFunc("/hello", helloHandler)
 	mux.HandleFunc("/about", aboutHandler)
+	mux.HandleFunc("/products", getProductsHandler)
 
 	fmt.Print("Starting server on :3000\n")
 
@@ -28,4 +54,15 @@ func main(){
 		fmt.Print("Error starting server:", err)
 	}
 	// http.ListenAndServe(":3000", mux)
+}
+
+func init(){
+	prd1 := Product{ID: 1, Title: "Product 1", Price: 10.0, Description: "Description for product 1", ImgUrl: "https://via.placeholder.com/150"}	
+	prd2 := Product{ID: 2, Title: "Product 2", Price: 20.0, Description: "Description for product 2", ImgUrl: "https://via.placeholder.com/150"}
+	prd3 := Product{ID: 3, Title: "Product 3", Price: 30.0, Description: "Description for product 3", ImgUrl: "https://via.placeholder.com/150"}
+	prd4 := Product{ID: 4, Title: "Product 4", Price: 40.0, Description: "Description for product 4", ImgUrl: "https://via.placeholder.com/150"}
+	prd5 := Product{ID: 5, Title: "Product 5", Price: 50.0, Description: "Description for product 5", ImgUrl: "https://via.placeholder.com/150"}
+	prd6 := Product{ID: 6, Title: "Product 6", Price: 60.0, Description: "Description for product 6", ImgUrl: "https://via.placeholder.com/150"}
+
+	productList = append(productList, prd1, prd2, prd3, prd4, prd5, prd6)
 }
